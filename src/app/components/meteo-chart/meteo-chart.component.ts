08c2502499028c404327b48161d6fa963b0a2c1d
Damiano <d.dilionardo@gmail.com>
2023-10-09 21:57:39 +0200

    u

--- src/app/components/meteo-chart/meteo-chart.component.ts
+++ src/app/components/meteo-chart/meteo-chart.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-meteo-chart',
   templateUrl: './meteo-chart.component.html',
   styleUrls: ['./meteo-chart.component.scss']
 })
-export class MeteoChartComponent {
+export class MeteoChartComponent implements OnInit{
+
+  ngOnInit(): void {
+
+  }
 
 }
